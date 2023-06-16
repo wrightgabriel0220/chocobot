de7c81588d8e540cbfa05cf0276ea9a79639e6bd
wrightgabriel0220 <gabrielthomas.wright@gmail.com>
2023-06-16 03:30:58 -0700

    implements join and leave commands

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands, tasks
 import os
@@ -7,8 +8,77 @@ import youtube_dl
 load_dotenv()
 
 # Get the API token from the .env file
-DISCORD_TOKEN = os.gotenv("discord_token")
+DISCORD_TOKEN = os.getenv("discord_token")
+COMMAND_PREFIX = os.getenv("command_prefix")
 
-intents = discord.Intents().all()
+
+# Setting the intents. These should match the intents on the Discord Developer Portal
+intents = discord.Intents.all()
+
+
+# Connecting to an individual client of Chocobot
 client = discord.Client(intents = intents)
-bot = commands.Bot(command_prefix = '!', intent = intents)
+# Creates a reference to our new Chocobot client
+bot = commands.Bot(command_prefix = COMMAND_PREFIX, intents = intents)
+
+# ytdl configuration
+youtube_dl.utils.bug_reports_message = lambda: 'There was an error!'
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+# ffmpeg configuration
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+
+# Initializing our ytdl client
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume = 0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop = None, stream = False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download = not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+    
+@bot.command(name = 'join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send('f{ctx.message.author.name} is not connected to a voice channel')
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name = 'leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send('The bot is connected to a voice channel.')
+
+if __name__ == "__main__":
+    bot.run(DISCORD_TOKEN)
